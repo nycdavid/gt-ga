a48b08b640d85dfd893e523051a9e1a1458a8927
David Ko <davidsj.ko@gmail.com>
2022-12-20 09:54:19 -0500

    initial commit

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(fib2(100))
+}
+
+func fib1(n int) int {
+	if n == 0 {
+		return 0
+	}
+
+	if n == 1 {
+		return 1
+	}
+
+	return fib1(n-1) + fib1(n-2)
+}
+
+func fib2(n int) int {
+	F := make([]int, n+1)
+
+	F[0] = 0
+	F[1] = 1
+
+	for i := 2; i <= n; i++ {
+		F[i] = F[i-1] + F[i-2]
+	}
+
+	return F[n]
+}
